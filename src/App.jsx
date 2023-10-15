daa8967e4f69eacc5af42b095b048c83acd89968
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-10-15 13:30:30 +0530

    SETUP Comp , Pages

--- src/App.jsx
+++ src/App.jsx
@@ -34,6 +34,7 @@ function App() {
 
   return (
     <BrowserRouter>
+      <Header/>
       <Routes>
         <Route path="/"  element ={<Home/>}/>
         <Route path="/:mediaType/:id"  element ={<Details/>}/>
@@ -41,6 +42,7 @@ function App() {
         <Route path="/explore/:mediaType"  element ={<Explore/>}/>
         <Route path="*"  element ={<pageNotFound/>}/>
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
