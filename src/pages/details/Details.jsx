8145fb051764c8ee59aadbb7c0531943a602bc1b
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-10-28 19:46:46 +0530

    Video section

--- src/pages/details/Details.jsx
+++ src/pages/details/Details.jsx
@@ -1,6 +1,8 @@
 import { useParams } from "react-router-dom"
 import useFetch from "../../hooks/useFetch"
 import DetailsBanner from "./DetailsBanner/DetailsBanner"
+import Cast from "./cast/Cast";
+import VideosSection from "./video/VideosSection";
 
 
 export default function Details() {
@@ -13,7 +15,8 @@ export default function Details() {
     <div>
       
       <DetailsBanner video = {data?.results?.[0]}  crew = {credits?.crew} />
-
+        <Cast data={credits?.cast} loading={creditsLoading}/>
+        <VideosSection data={data} loading={loading}/>
     </div>
   )
 }
