004f630079b8f8483bd03c1f8fb5f49ac75a824a
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-10-30 19:49:37 +0530

    Run build and deploy

--- src/pages/home/Popular/Popular.jsx
+++ src/pages/home/Popular/Popular.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 
-import Carousel from "../../../components/carousel/Carousel";
+import Carousel from "../../../components/carousel/Carousel.jsx";
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper";
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs";
 
