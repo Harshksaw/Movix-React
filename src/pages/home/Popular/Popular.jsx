30cf08caf55528580256ec546c725ea9feeda011
Harsh kumar <95240459+Harshksaw@users.noreply.github.com>
2023-10-30 19:27:24 +0530

    Run build and deploy

--- src/pages/home/Popular/Popular.jsx
+++ src/pages/home/Popular/Popular.jsx
@@ -2,7 +2,7 @@ import { useState } from "react"
 import ContentWrapper from "../../../components/contentWrapper/ContentWrapper.jsx"
 import SwitchTabs from "../../../components/switchTabs/SwitchTabs.jsx"
 import useFetch from "../../../hooks/useFetch"
-import Carousel from '../../../components/carousel/Carousel.jsx';
+import Carousel from "../../../components/carousel/Carousel.jsx"
 
 
 
@@ -22,7 +22,7 @@ export default function Popular() {
     return (
         <div className="carouselSection">
             <ContentWrapper>
-                <span className="carouselTitle">What's Popular</span>
+                <span className="carouselTitle">What s Popular</span>
                 <SwitchTabs data={["Movies", "TV shows"]} onTabChange={onTabChange} />
             </ContentWrapper>
             <Carousel data={data?.results} loading={loading} endpoint= {endpoint}/>
